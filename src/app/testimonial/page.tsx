e41c4e16cd02a009a93d77507c1c11e9ce1539be
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-14 16:51:11 +0500

    creating testimonial section

---
+++ src/app/testimonial/page.tsx
@@ -0,0 +1,21 @@
+import SubHeading from '@/components/SubHeading'
+import React from 'react'
+
+const TestimonialPage = () => {
+    return (
+        <div className='w-full h-auto flex justify-center items-center lg:py-20 md:py-14 sm:py-7'>
+            <div className='lg:w-[85%] md:w-[95%] w-full md:px-0 px-3  h-auto flex flex-col lg:gap-16 md:gap-12 gap-8 justify-center items-center'>
+                <div className="heading flex flex-col gap-6 items-center justify-center h-auto w-full">
+                    <SubHeading title="Testimonials" />
+                    <h2 className='text-6xl font-mono text-center'>What talk about us</h2>
+                </div>
+
+<div className='w-full h-auto bg-secondary flex justify-center items-center'>
+    
+</div>
+            </div>
+        </div>
+    )
+}
+
+export default TestimonialPage
