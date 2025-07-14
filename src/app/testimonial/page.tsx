69d6d3e01f768012f596540452c14d2acdc8327e
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-14 17:16:29 +0500

    creating testimonial section

--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -1,7 +1,39 @@
+'use client'
 import SubHeading from '@/components/SubHeading'
-import React from 'react'
+import React, { useState } from 'react'
 
 const TestimonialPage = () => {
+    let [active, setActive] = useState(0)
+    let testimonial_data = [
+        {
+            title: 'Family Custody Battle',
+            testimonial: '"Vanguard Legal Group helped me through a difficult divorce with professionalism and care. I couldn’t have asked for better representation."',
+            name: 'Sarah T.',
+            image: '/images/client-image.jpg'
+
+        },
+        {
+            title: 'Corporate Acquisition Case',
+            testimonial: '"Vanguard Legal Group helped me through a difficult divorce with professionalism and care. I couldn’t have asked for better representation."',
+            name: 'Sarah T.',
+            image: '/images/client-image.jpg'
+
+        },
+        {
+            title: 'High-Profile Criminal Defense',
+            testimonial: '"Vanguard Legal Group helped me through a difficult divorce with professionalism and care. I couldn’t have asked for better representation."',
+            name: 'Sarah T.',
+            image: '/images/client-image.jpg'
+
+        },
+        {
+            title: 'Personal Injury Settlement',
+            testimonial: '"Vanguard Legal Group helped me through a difficult divorce with professionalism and care. I couldn’t have asked for better representation."',
+            name: 'Sarah T.',
+            image: '/images/client-image.jpg'
+
+        },
+    ]
     return (
         <div className='w-full h-auto flex justify-center items-center lg:py-20 md:py-14 sm:py-7'>
             <div className='lg:w-[85%] md:w-[95%] w-full md:px-0 px-3  h-auto flex flex-col lg:gap-16 md:gap-12 gap-8 justify-center items-center'>
@@ -13,8 +45,11 @@ const TestimonialPage = () => {
                 <div className='w-full h-auto bg-secondary py-10 flex justify-center items-center'>
                     <div className="w-[90%] flex gap-6 ">
                         <ul className='w-[30%] h-auto flex flex-col gap-6'>
-<li className='text-2xl font-sans text-black/50 focus:text-black hover:text-black active:text-black'></li>
+                            {testimonial_data.map(({ title }, index) => (
+                                <li key={index} onClick={()=> setActive} className='text-2xl font-sans text-black/50 focus:text-black hover:text-black active:text-black'>{title}</li>
+                            ))}
                         </ul>
+                        <div className=''></div>
                     </div>
                 </div>
             </div>
